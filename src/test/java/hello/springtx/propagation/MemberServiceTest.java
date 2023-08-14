94a6c1a414fef112417260179b08e85ad0dc3d45
foxworld <foxworld@DESKTOP-FOXWORLD>
2023-08-14 20:56:41 +0900

    로그예외_outerTxOff_fail

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package hello.springtx.propagation;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -35,5 +36,22 @@ public class MemberServiceTest {
 		assertTrue(memberRespository.find(username).isPresent());
 		assertTrue(logRepository.find(username).isPresent());
 	}
+	
+	/*
+	 * memberService    @Transaction:off
+	 * memberRepository @Transaction:on
+	 * logRepository    @Transaction:on
+	 */
+	
+	@Test
+	void outerTxOff_fail() {
+		//given
+		String username = "로그예외_outerTxOff_fail";
+		//when
+		assertThatThrownBy(() -> memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+		//then 모든데이터가 저장된다
+		assertTrue(memberRespository.find(username).isPresent());
+		assertTrue(logRepository.find(username).isEmpty());
+	}	
 
 }
